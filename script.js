6c0c6e2e0394e1af13099de5e0d73037c29ef31d
b-blots-tran <brian.tran0121@gmail.com>
2023-07-31 14:11:34 -0400

    start hooking up event listeners to  buttons

--- script.js
+++ script.js
@@ -5,8 +5,60 @@ import {
   divide,
   multiply,
 } from "./utility/operations.js";
-let chosenOperation = add;
-let firstNumber = 0;
-let secondNumber = 0;
+// i need a stack or something to keep track of numbers and operations
+let chosenOperation;
+let firstNumber = "";
+let secondNumber = "";
+let displayString = "";
+let currentInputDiv = document.getElementById("current-input");
+let lastInputDiv = document.getElementById("last-input");
 
-let answer = operate(chosenOperation, firstNumber, secondNumber);
+let numbersArray = document.querySelectorAll(".number");
+let clearBtn = document.getElementById("clear");
+let negateBtn = document.getElementById("negate");
+let dotBtn = document.getElementById("dot");
+let addBtn = document.getElementById("add");
+let subtractBtn = document.getElementById("subtract");
+let multiplyBtn = document.getElementById("multiply");
+let divideBtn = document.getElementById("divide");
+
+numbersArray.forEach((numberBtn) => {
+  numberBtn.addEventListener("click", (e) => {
+    if (firstNumber.length < 15) {
+      let btnNumberPressed = numberBtn.id;
+      let accumulatedNumber = firstNumber;
+      currentInputDiv.textContent = accumulatedNumber + btnNumberPressed;
+      firstNumber = accumulatedNumber + btnNumberPressed;
+    }
+  });
+});
+
+clearBtn.addEventListener("click", (e) => {
+  currentInputDiv.textContent = 0;
+  lastInputDiv.textContent = 0;
+  lastInputDiv.style.visibility = "hidden";
+  firstNumber = "";
+  secondNumber = "";
+});
+
+dotBtn.addEventListener("click", (e) => {
+  if (firstNumber.length < 15 && !firstNumber.includes(".")) {
+    if (firstNumber.length == 0) {
+      firstNumber += "0";
+    }
+    firstNumber += ".";
+    currentInputDiv.textContent = firstNumber;
+  }
+});
+
+addBtn.addEventListener("click", (e) => {
+  if (firstNumber.length > 0 && secondNumber.length == 0) {
+    secondNumber = firstNumber;
+    displayString = secondNumber + " + ";
+    lastInputDiv.textContent = displayString;
+    lastInputDiv.style.visibility = "visible";
+    firstNumber = "";
+    chosenOperation = add;
+  } else {
+  }
+});
